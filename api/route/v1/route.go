e5db543adef390a58bb433e3e3077c78b4ca9ccc
CGW1996 <s22760081@gmail.com>
2023-05-21 06:33:45 +0800

    feat: jwt middleware

--- api/route/v1/route.go
+++ api/route/v1/route.go
@@ -3,6 +3,7 @@ package route
 import (
 	"time"
 
+	"github.com/CGW1996/golang-backend/api/middleware"
 	"github.com/CGW1996/golang-backend/bootstrap"
 	"github.com/CGW1996/golang-backend/mongo"
 	"github.com/gin-gonic/gin"
@@ -13,4 +14,9 @@ func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, routerV
 	NewSignupRouter(env, timeout, db, publicRouterV1)
 	NewLoginRouter(env, timeout, db, publicRouterV1)
 	NewRefreshTokenRouter(env, timeout, db, publicRouterV1)
+
+	protectedRouterV1 := routerV1.Group("")
+	// middleware to verify accessToken
+	protectedRouterV1.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
+	// all private api
 }
